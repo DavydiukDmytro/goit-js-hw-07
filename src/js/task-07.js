dd05596782b7295d686bad089b29850f0d49aa1f
davudyuck <davudyuck0501@gmail.com>
2023-02-28 19:24:52 +0200

    Setting Repository

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-    inputControl: document.querySelector('#font-size-control'),
-    spanText: document.querySelector('#text')
-}
-const abracadabra = event => {
-    refs.spanText.style.fontSize = `${event.currentTarget.value}px`;
-};
-refs.inputControl.addEventListener("input", abracadabra);
